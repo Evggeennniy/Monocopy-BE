5cdb8d4dbbcef9d0083925bd5dc251fcb0c84cde
Evgeniy <myiphone128@mail.ru>
2025-10-02 20:29:18 +0300

    fixes

--- app/settings/settings.py
+++ app/settings/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 
     'rest_framework',
+    "rest_framework_simplejwt",
     "drf_spectacular",
     "drf_spectacular_sidecar",
     'drf_yasg',
@@ -134,4 +135,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
 }
