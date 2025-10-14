314ab46e33ec8edc59c208be364106e674d7a5ca
Evgeniy <myiphone128@mail.ru>
2025-10-14 14:18:48 +0300

    added ip

--- app/settings/settings.py
+++ app/settings/settings.py
@@ -29,11 +29,13 @@ ALLOWED_HOSTS = ["shironenergy.com", "127.0.0.1",]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    'http://192.168.0.115:5173',
     "https://shironenergy.com",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
+    'http://192.168.0.115:5173',
     "https://shironenergy.com",
 ]
 
