a7a299e226c18edfa857b7fedabc604e75599569
Evgeniy <myiphone128@mail.ru>
2025-10-18 16:14:45 +0300

    media url

--- app/settings/settings.py
+++ app/settings/settings.py
@@ -146,6 +146,8 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / '..' / "staticfiles"
+MEDIA_URL = "media/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
